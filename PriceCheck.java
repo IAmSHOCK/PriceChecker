fcd6dfbc51d347f016e9f45b5400561a96b81cac
IAmSHOCK <joaoaires98@gmail.com>
2021-07-08 02:45:16 +0100

    more tests

--- PriceCheck.java
+++ PriceCheck.java
@@ -5,16 +5,7 @@ import java.net.*;
 
 public class PriceCheck{
 	public static void main(String[] argv) {
-		try  {  
-			Desktop d = Desktop.getDesktop();
-			File f = new File(argv[0]);    
-			if(f.exists())         //checks file exists or not  
-			d.open(f);              //opens the specified file  
-		}  
-		catch(Exception e) {  
-			e.printStackTrace();  
-		}
-	}
-}
-	
+		File f = new File(argv[0]);	
+		f.setReadOnly();
 		
+	}
